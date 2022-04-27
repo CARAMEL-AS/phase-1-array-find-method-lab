397342a4a56500dd23f65b6c222e188136c59d68
Aftab Sidhu <Aftab180@gmail.com>
2022-04-27 17:54:27 -0400

    commit tree ae2cec96a5be40c156f39888d7abfc1545814f70

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// // find()
+
+// function superbowlWin(wins) {
+//     wins.result === 'W'
+//     }
+// record.find(superbowlWin);
+
+
+const superbowlWin = (records) => {
+    const found = records.find(record => {
+          return record.result === 'W'
+    })
+    return found ? found.year : undefined;
+}
